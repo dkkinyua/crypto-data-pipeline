5b1a126ab7f2d71e511c39108dbded7ca19eb785
dkkinyua <denzelkinyua11@gmail.com>
2025-04-30 13:47:20 +0300

    Commit: load_to_db() function debugged. DAG now running normally

--- dags/extract_data.py
+++ dags/extract_data.py
@@ -1,18 +1,28 @@
 import os
 import json
 import requests
-from airflow.decorators import dag, task
-from airflow.operators.python import PythonOperator
+import pandas as pd
+from dotenv import load_dotenv
 from pendulum import datetime
+from datetime import timedelta
+from sqlalchemy import create_engine
+from airflow.decorators import dag, task
+from eazyetl import Load, Extract
+
+load_dotenv()
 
 API_KEY = os.getenv('CRYPTO_API_KEY')
+DB_CONNECTION_KEY = os.getenv('DB_CONNECTION_KEY')
+
+
 
 @dag(
     dag_id='crypto_data_dag',
     schedule='@daily',
     start_date=datetime(2025, 4, 22),
-    default_args={'owner': 'denzo', 'retries': 3},
+    default_args={'owner': 'denzo', 'retries': 3, 'retry_delay': timedelta(minutes=5)},
     tags = ['Crypto Data Pipeline DAG']
+    
 )
 
 def crypto_pipeline():
@@ -63,14 +73,36 @@ def crypto_pipeline():
             data = []
 
         data.extend(crypto_data)
-
         with open(filepath, 'w') as f:
             json.dump(data, f, indent=4)
-
         print('Data loaded successfully!')
 
+    @task
+    def clean_data(filepath):
+        try:
+            filepath = '/home/deecodes/airflow/data.json'
+            df = pd.read_json(filepath)
+            df = df.explode(['name', 'symbol', 'current_price', 'high_24h', 'low_24h', 'price_change_24h', 'last_updated'], ignore_index=True)
+            df.index = df.index + 1
+            df.to_csv('/home/deecodes/airflow/cleaned_data.csv')
+        except Exception as e:
+            print(f"Error: {str(e)}")
+
+    @task
+    def load_to_db(filepath):
+        try:
+            filepath = '/home/deecodes/airflow/cleaned_data.csv'
+            df = Extract.read_csv(filepath)
+            Load.load_to_db(df, name='crypto_data_daily', url=DB_CONNECTION_KEY)
+        except Exception as e:
+            print(f"Error: {str(e)}")
+        
+
     extract_process = extract_data()
-    load_data(extract_process)
+    unclean_file = load_data(extract_process)
+    file = clean_data(unclean_file)
+    load_to_db(file)
 
 crypto_dag = crypto_pipeline()
 
+# print(DB_CONNECTION_KEY)
